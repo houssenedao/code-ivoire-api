e22f45a16019fc96db7023cbc7aab9fb435ade29
Houssene Dao <dao.houssene@gmail.com>
2018-06-01 16:30:29 +0000

    Block user oauth/login with passport if user account is not activated or user is disabled

--- app/User.php
+++ app/User.php
@@ -7,6 +7,7 @@ use App\Models\Review;
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use League\OAuth2\Server\Exception\OAuthServerException;
 
 class User extends Authenticatable
 {
@@ -41,5 +42,24 @@ class User extends Authenticatable
     public function events()
     {
         return $this->belongsToMany(Event::class, 'event_tag');
+    }
+
+    /**
+     * @param $username
+     * @return mixed
+     * @throws OAuthServerException
+     */
+    public function findForPassport($username)
+    {
+        $user = $this->where('email', $username)->first();
+
+        if ($user !== null)
+            if ($user->activated === 0)
+                throw new OAuthServerException('User account is not activated', 6, 'Unauthorized', 401);
+
+            if ($user->deleted_at !== null)
+                throw new OAuthServerException('User account is disabled', 6, 'Unauthorized', 401);
+
+        return $user;
     }
 }
